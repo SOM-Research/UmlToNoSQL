dfa3847ee1d7c8e698f3c9f7b5d8f7ce62432a2f
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-03-08 01:53:45 +0100

    Remove Region related classes
    Regions are used during the code generation, not at runtime.

--- demo/src/main/java/som/umltonosql/demo/core/generated/DemoBootstrap.java
+++ demo/src/main/java/som/umltonosql/demo/core/generated/DemoBootstrap.java
@@ -1,21 +1,14 @@
 package som.umltonosql.demo.core.generated;
 
-import fr.inria.atlanmod.commons.log.Log;
 import som.umltonosql.core.Bootstrap;
-import som.umltonosql.core.bean.Bean;
+import som.umltonosql.core.LifeCycleManager;
 import som.umltonosql.core.constraint.Constraint;
 import som.umltonosql.core.constraint.ConstraintManager;
-import som.umltonosql.core.LifeCycleManager;
 import som.umltonosql.core.datastore.handler.DrillHandler;
 import som.umltonosql.core.datastore.handler.MongoHandler;
 import som.umltonosql.core.datastore.query.DrillQuery;
 import som.umltonosql.core.datastore.query.MongoQuery;
-import som.umltonosql.core.datastore.query.QueryResult;
-import som.umltonosql.core.datastore.query.processor.DrillQueryProcessor;
-import som.umltonosql.core.datastore.query.processor.MongoQueryProcessor;
 import som.umltonosql.core.datastore.store.MongoDatastore;
-import som.umltonosql.core.region.ModelRegionManager;
-import som.umltonosql.core.region.Region;
 import som.umltonosql.demo.mongodb.beans.Order;
 import som.umltonosql.demo.mongodb.beans.Product;
 
@@ -31,11 +24,6 @@ public class DemoBootstrap extends Bootstrap {
 
         // Creates the Datastores
         MongoDatastore businessDatastore = new MongoDatastore("demo");
-
-        // Create the Regions
-        Region businessRegion = new Region("business", businessDatastore);
-        ModelRegionManager regionManager = new ModelRegionManager();
-        regionManager.addRegion(businessRegion);
 
         middleware = new DemoMiddleware(businessDatastore);
 
