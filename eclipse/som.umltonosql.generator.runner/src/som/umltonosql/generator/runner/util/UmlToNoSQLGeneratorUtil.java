da1d6adf68696e437e4e9b09b2e835d49aaa98fa
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-04-12 15:30:31 +0200

    Add DatabaseKind checking in Generator bindings
    This allows to use the same DocumentDB metamodel in multiple generators dedicated to Document Database implementations.

--- eclipse/som.umltonosql.generator.runner/src/som/umltonosql/generator/runner/util/UmlToNoSQLGeneratorUtil.java
+++ eclipse/som.umltonosql.generator.runner/src/som/umltonosql/generator/runner/util/UmlToNoSQLGeneratorUtil.java
@@ -13,6 +13,8 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
+import documentdb.Database;
+import documentdb.DatabaseKind;
 import documentdb.DocumentdbPackage;
 import postgres.PostgresPackage;
 import region.Partition;
@@ -64,7 +66,12 @@ public class UmlToNoSQLGeneratorUtil {
 			StorageKind sKind = r.getStorage();
 			if(sKind.equals(StorageKind.DOCUMENT)) {
 				Resource mongoResource = resourceSet.getResource(URI.createURI("model/" + r.getName().toLowerCase() + ".xmi"), true);
-				generators.add(new UmlToNoSQLMongoGenerator(mongoResource, new File(basePackage, r.getName().toLowerCase()), r));
+				Database db = (Database)mongoResource.getContents().get(0);
+				if(db.getRawDatabase().equals(DatabaseKind.MONGODB)) {
+					generators.add(new UmlToNoSQLMongoGenerator(mongoResource, new File(basePackage, r.getName().toLowerCase()), r));
+				} else {
+					throw new RuntimeException("Unknown Document Database Kind: " + db.getRawDatabase());
+				}
 			} else if(sKind.equals(StorageKind.RELATIONAL)) {
 				Resource postgresResource = resourceSet.getResource(URI.createURI("model/" + r.getName().toLowerCase() + ".xmi"), true);
 				generators.add(new UmlToNoSQLPostgresGenerator(postgresResource, new File(basePackage, r.getName().toLowerCase()), r));
