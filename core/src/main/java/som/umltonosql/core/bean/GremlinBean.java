98a5cc6cffc49455e0e527940d2ee39b5119b0b2
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-05-04 14:19:37 +0200

    Add generic GremlinBean methods and corresponding GremlinDatastore ones

--- core/src/main/java/som/umltonosql/core/bean/GremlinBean.java
+++ core/src/main/java/som/umltonosql/core/bean/GremlinBean.java
@@ -1,11 +1,34 @@
 package som.umltonosql.core.bean;
 
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import som.umltonosql.core.datastore.store.GremlinDatastore;
 
+/**
+ * A Gremlin-based {@link Bean} implementation.
+ * <p>
+ * This class delegates {@link Bean}-level operations to a {@link GremlinDatastore} that managed the underlying
+ * database.
+ */
 public class GremlinBean extends Bean<GremlinDatastore> {
 
     public GremlinBean(String id, GremlinDatastore gremlinDatastore) {
         super(gremlinDatastore);
         this.id = id;
+    }
+
+    protected void setAttribute(String field, Object value) {
+        datastore.setValue(id, this.getClass(), field, value);
+    }
+
+    protected <T> T getAttribute(String field) {
+        return (T) datastore.getValue(id, this.getClass(), field);
+    }
+
+    protected Iterable<Vertex> getAssociation(String field, Class<? extends Bean> fieldClazz) {
+        return datastore.getAssociation(id, this.getClass(), field, fieldClazz);
+    }
+
+    protected void addAssociation(String field, Bean value) {
+        datastore.addAssociation(id, this.getClass(), field, value);
     }
 }
--- core/src/main/java/som/umltonosql/core/datastore/store/GremlinDatastore.java
+++ core/src/main/java/som/umltonosql/core/datastore/store/GremlinDatastore.java
@@ -1,11 +1,15 @@
 package som.umltonosql.core.datastore.store;
 
 import fr.inria.atlanmod.commons.log.Log;
+import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
 import org.bson.types.ObjectId;
 import som.umltonosql.core.bean.Bean;
+import som.umltonosql.core.bean.GremlinBean;
+import som.umltonosql.core.exceptions.ConsistencyException;
 import som.umltonosql.core.exceptions.LifeCycleException;
 
 import java.io.File;
@@ -50,6 +54,7 @@ public class GremlinDatastore extends Datastore {
         return graph;
     }
 
+    @Override
     public <T extends Bean> T createElement(Class<T> clazz) {
         ObjectId newObjectId = new ObjectId();
         Vertex vertex = graph.addVertex(getVertexLabelFromBeanClass(clazz));
@@ -72,20 +77,20 @@ public class GremlinDatastore extends Datastore {
     public <T extends Bean> T getElement(String id, Class<T> clazz) {
         Vertex vertex = null;
         try {
-            vertex = graph.traversal().V().hasLabel(getVertexLabelFromBeanClass(clazz)).has("_id", id).next();
-        } catch(NoSuchElementException e) {
+            vertex = getVertex(id, clazz);
+        } catch (NoSuchElementException e) {
             /*
-             * The database does not contain the element.
+             * The database does not contain a Vertex with the provided id, there is no Bean instance to construct.
              */
             return null;
         }
         try {
             Constructor<?> constructor = clazz.getConstructor(String.class, GremlinDatastore.class);
             return (T) constructor.newInstance(id, this);
-        } catch(NoSuchMethodException e) {
+        } catch (NoSuchMethodException e) {
             Log.error("Cannot find the constructor for the provided bean {0}", clazz.getName());
             throw new RuntimeException(e);
-        } catch(InstantiationException | IllegalAccessException | InvocationTargetException e1) {
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e1) {
             Log.error("Cannot invoke the constructor for the provided bean {0}", clazz.getName());
             throw new RuntimeException(e1);
         }
@@ -97,6 +102,99 @@ public class GremlinDatastore extends Datastore {
         graph.traversal().V().hasLabel(getVertexLabelFromBeanClass(clazz)).toStream()
                 .forEach(v -> beans.add(getElement(v.value("_id"), clazz)));
         return beans;
+    }
+
+    public void setValue(String id, Class<? extends Bean> clazz, String field, Object value) {
+        Vertex v = null;
+        try {
+            v = getVertex(id, clazz);
+        } catch(NoSuchElementException e) {
+            throw new ConsistencyException(MessageFormat.format("Cannot retrieve the vertex with the provided ID: " +
+                    "{0}", id), e);
+        }
+        v.property(field, value);
+    }
+
+    public Object getValue(String id, Class<? extends Bean> clazz, String field) {
+        Vertex v = null;
+        try {
+            v = getVertex(id, clazz);
+        } catch (NoSuchElementException e) {
+            throw new ConsistencyException(MessageFormat.format("Cannot retrieve the vertex with the provided ID: " +
+                    "{0}", id), e);
+        }
+        return v.property(field);
+    }
+
+    public Iterable<Vertex> getAssociation(String id, Class<? extends Bean> clazz, String field, Class<? extends
+            Bean> fieldClazz) {
+        // TODO handle exception
+        Vertex v = null;
+        try {
+            getVertex(id, clazz);
+        } catch (NoSuchElementException e) {
+            throw new ConsistencyException(MessageFormat.format("Cannot retrieve the vertex with the provided ID: " +
+                    "{0}", id), e);
+        }
+        if (GremlinBean.class.isAssignableFrom(fieldClazz)) {
+            /*
+             * The value is stored in this datastore, we can retrieve it by navigating the outgoing edges.
+             */
+            return () -> v.vertices(Direction.IN, field);
+        } else {
+            /*
+             * The value is stored in another datastore, we can retrieve its IDs in the vertex's properties.
+             */
+            return () -> v.value(field);
+        }
+    }
+
+    public void addAssociation(String id, Class<? extends Bean> clazz, String field, Bean value) {
+        Vertex fromVertex = null;
+        try {
+            fromVertex = getVertex(id, clazz);
+        } catch(NoSuchElementException e) {
+            throw new ConsistencyException(MessageFormat.format("Cannot retrieve the input vertex of the " +
+                    "association {0} with the provided ID: {1}", field, id),e);
+        }
+        if (value instanceof GremlinBean) {
+            /*
+             * The value to add is stored in this datastore, we can retrieve the corresponding vertex and create an
+             * edge representing the association.
+             */
+            Vertex toVertex = null;
+            try {
+                toVertex = getVertex(value.getId(), value.getClass());
+            } catch(NoSuchElementException e) {
+                throw new ConsistencyException(MessageFormat.format("Cannot retrieve the output vertex of the " +
+                        "association {0} with the provided ID: {0}", field, id),e);
+            }
+            fromVertex.addEdge(field, toVertex);
+        } else {
+            /*
+             * The value to add is not stored in this datastore, we add its ID to the vertex's properties.
+             */
+            VertexProperty<List<String>> toIdsProperty = fromVertex.property(field);
+            if(toIdsProperty.isPresent()) {
+                toIdsProperty.value().add(value.getId());
+            } else {
+                List<String> toIds = new ArrayList<>();
+                toIds.add(value.getId());
+                fromVertex.property(field, toIds);
+            }
+        }
+    }
+
+    /**
+     * Returns the database {@link Vertex} representing an instance of {@code clazz} with the provided {@code id}.
+     *
+     * @param id    the unique identifier of the {@link Vertex} to retrieve
+     * @param clazz the {@link Class} of the {@link GremlinBean} to retrieve an instance of
+     * @return the database {@link Vertex} representing an instance of {@code clazz} with the provided {@code id}
+     * @throws NoSuchElementException if the database does not contain a {@link Vertex} with the provided {@code id}
+     */
+    private Vertex getVertex(String id, Class<? extends Bean> clazz) throws NoSuchElementException {
+        return graph.traversal().V().hasLabel(getVertexLabelFromBeanClass(clazz)).has("_id", id).next();
     }
 
     @Override
